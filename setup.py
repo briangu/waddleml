3c6b4248930e3dbfc418105e60266ad8871cfffc
Brian Guarraci <brian@ops5.com>
2024-09-15 10:10:03 -0700

    fix setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(
+    name='waddleml',
+    packages=find_packages(),
+    version='0.1.0',
+    description='WaddleML is a machine learning stats tracker',
+    author='Brian Guarraci',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=['duckdb'],
+    python_requires='<=3.12,>=3.8',
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='tests',
+    scripts=['scripts/waddle']
+)
